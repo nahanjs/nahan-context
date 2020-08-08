190940a99e51dbef7641e9f9467a376c3db27397
LabMemNo003 <LabMemNo003@gmail.com>
2020-08-08 21:34:57 +0800

    Add init_path()

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const init_path = require('./lib/path');
+
 function Context() {
 
     async function context(ctx, next) {
+
+        init_path(ctx);
+
         await next();
     };
 
